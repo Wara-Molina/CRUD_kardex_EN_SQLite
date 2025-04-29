5ed61775fe26a4ec0af4f63fc801953f2a28357a
Frehisy Molina <upwc24@gmail.com>
2025-04-29 16:35:18 -0400

    segundo commit

--- app.py
+++ app.py
@@ -66,8 +66,46 @@ def personas_save():
     conn.close()
     return redirect('/personas')
     
+# editar persona
+@app.route("/personas/edit/<int:id>")
+def personas_edit(id):
+    conn = sqlite3.connect("kardex.db")
+    conn.row_factory = sqlite3.Row
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM personas WHERE id=?",(id,))
+    persona = cursor.fetchone()
+    conn.close()
+    return render_template("personas/edit.html", persona = persona)
+
+#grabar los datos
+@app.route("/personas/update", methods = ['POST'])
+def personas_update():
+    id = request.form['id']
+    nombre = request.form['nombre']
+    telefono = request.form['telefono']
+    fecha_nac = request.form['fecha_nac']
+    
+    conn = sqlite3.connect("kardex.db")
+    cursor = conn.cursor()
+    
+    cursor.execute("UPDATE personas SET nombre = ?, telefono=?, fecha_nac =? WHERE id=?", (nombre,telefono,fecha_nac,id))
+    
+    conn.commit()
+    conn.close()
+    return redirect("/personas")
+
+# eliminacion de datos
+@app.route("/personas/delete/<int:id>")
+def personas_delete(id):
+    conn = sqlite3.connect("kardex.db")
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM personas WHERE id=?",(id,))
+    conn.commit()
+    conn.close()
+    return redirect('/personas')
 
 
+    
 
 if __name__=="__main__":
     app.run(debug=True)
